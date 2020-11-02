e3ae1de0b8e569fcc043d120389ab1330c4b0ff7
AsemBasheer <AsemBasheer@users.noreply.github.com>
2020-11-02 15:56:50 +0200

    Test commit. Please ignore

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-
-
+document.addEventListener('DOMContentLoaded',  ()=> {
+    const cells= document.querySelectorAll('.cell')
+    console.log(cells)
+    cells.forEach((cell,i)=>{
+        cell.addEventListener('click',()=>{
+            console.log(i)
+            cells[i].classList.add('clickedX')
+        })
+    })
+});
