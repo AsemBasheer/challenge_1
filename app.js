25225bd27333adc432801ea98a3ef76ed10facf6
AsemBasheer <AsemBasheer@users.noreply.github.com>
2020-11-02 17:49:06 +0200

    Test commit. Please ignore // styling and reset

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-document.addEventListener('DOMContentLoaded',  ()=> {
-    const cells= document.querySelectorAll('.cell')
-    console.log(cells)
-    cells.forEach((cell,i)=>{
-        cell.addEventListener('click',()=>{
-            console.log(i)
-            cells[i].classList.add('clickedX')
+document.addEventListener('DOMContentLoaded', () => {
+    const cells = document.querySelectorAll('.cell')
+    const player = document.querySelector('#playerTurn')
+    let currentPlayer = "Player1"
+    cells.forEach((cell, i) => {
+        cell.addEventListener('click', () => {
+            if (cells[i].classList[cells[i].classList.length - 1] === 'Player1' ||
+            cells[i].classList[cells[i].classList.length - 1] === 'Player2') {
+                console.log("no")
+            } else {
+                cells[i].classList.add(currentPlayer)
+                if (currentPlayer === 'Player1') {
+                    currentPlayer = 'Player2'
+                } else {
+                    currentPlayer = 'Player1'
+                }
+                player.innerHTML = `It's ${currentPlayer} trun`
+            }
+        })
+    })
+    const button = document.querySelector('.w3-button')
+    button.addEventListener('click',()=>{
+        cells.forEach((cell, i) => {
+            cell.classList.remove('Player1')
+            cell.classList.remove('Player2')
+            player.innerHTML = ''
+            currentPlayer = 'Player1'
         })
     })
 });
